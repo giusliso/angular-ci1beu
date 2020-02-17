38dd40f4e85fd2c293400f00951b50f275693f63
giusliso <giusliso@gmail.com>
2020-02-17 12:35:02 +0100

    5 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { DxDataGridModule } from "devextreme-angular";
+import { CommonModule } from "@angular/common";
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+   imports: [BrowserModule, DxDataGridModule, CommonModule],
+   declarations: [AppComponent],
+   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
